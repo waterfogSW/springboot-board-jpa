424a5e4c521cb91ff9b749666540b1935c17d4e2
San Kim <waterfog9580@gmail.com>
2022-06-13 17:56:58 +0900

    :recycle: ControllerAdvisor RuntimeException 처리 추가

--- src/main/java/com/waterfogsw/springbootboardjpa/common/exception/RestControllerAdvisor.java
+++ src/main/java/com/waterfogsw/springbootboardjpa/common/exception/RestControllerAdvisor.java
@@ -1,5 +1,7 @@
 package com.waterfogsw.springbootboardjpa.common.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -25,4 +27,8 @@ public class RestControllerAdvisor {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
     }
 
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
 }
